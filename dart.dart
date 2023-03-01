31c284ddda31c5f6d54eec2c1d4408d5ec8fad80
ShayanSir <125733674+ShayanSir@users.noreply.github.com>
2023-03-02 01:04:33 +0500

    Add files via upload

---
+++ dart.dart
@@ -0,0 +1,101 @@
+void main() {
+// Question no 01//
+  // print("Enter a number:");
+  // int num = int.parse(stdin.readLineSync()!);
+  // int num1 = 1;
+
+  // for (int i = 1; i <= num; i++) {
+  //   num1 *= i;
+  // }
+
+  // print("The factorial of $num is $num1");
+
+// ----*------*------*-----*-
+
+// Question no 02//
+  // List<int> numbers = [12, 34, 56, 16, 89, 67, 43];
+  // int highest = numbers[0];
+  // for (var i = 1; i < numbers.length; i++) {
+  //   if (numbers[i] > highest) {
+  //     highest = numbers[i];
+  //   }
+  // }
+  // print('The highest number from the list is $highest.');
+// ----*------*------*-----*-
+
+// Question no 03//
+  // Initialize the first number in the series to 0
+  // int num = 0;
+
+  // Iterate 10 times and print each number in the series
+  // for (int i = 1; i <= 10; i++) {
+  //   print(num);
+  //   num += 3;
+  // }
+// *--------*-------*-
+
+// Question no 04//
+  // for (var i = 0; i < 6; i++) {
+  //   print('**********');
+  // }
+
+  // for (var i = 1; i <= 8*5; i++) {
+  //   print('*' * i);
+  // }
+
+  // for (var i = 1; i <= 10; i += 2) {
+  //   print('123456789' * i);
+  // }
+
+  // for (var i = 1; i <= 10; i++) {
+  //   print('${i}' * (2 * i - 1));
+  // }
+// *-----*-------*----*--
+
+// Question no 05//
+  // int sum = 0;
+  // for (var i = 1; i <= 100; i += 2) {
+  //   sum += i;
+  // }
+  // print('The sum of all odd numbers between 1 and 100 is $sum.');
+  // ----*------*------*-----*-
+
+// Question no 06//
+  // List<int> numbers = [12, 34, 56, 16, 89, 67, 43];
+  // int length = numbers.length;
+  // for (var i = 0; i < length ~/ 2; i++) {
+  //   int temp = numbers[i];
+  //   numbers[i] = numbers[length - i - 1];
+  //   numbers[length - i - 1] = temp;
+  // }
+  // print(numbers);
+  // ----*------*------*-----*-
+
+// Question no 07//
+  // int a = 0, b = 1;
+  // for (int i = 0; a <= 100; i++) {
+  //   print("$a, ");
+  //   int nextNumber = a + b;
+  //   a = b;
+  //   b = nextNumber;
+  // }
+  // ----*------*------*-----*-
+
+// Question no 08//
+  // List<int> numbers = [12, 34, 56, 16, 89, 67, 43];
+  // int n = numbers.length;
+
+  // for (int i = 0; i < n - 1; i++) {
+  //   for (int j = i + 1; j < n; j++) {
+  //     if (numbers[i] > numbers[j]) {
+  //       int temp = numbers[i];
+  //       numbers[i] = numbers[j];
+  //       numbers[j] = temp;
+  //     }
+  //   }
+  // }
+
+  // print(numbers);
+
+// ----*------*------*-----*-
+}
